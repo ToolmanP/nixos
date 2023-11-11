d536bdbbfb17df26852b7484bb3b5d9b682ad3cd
Yiyang Wu <toolmanp@outlook.com>
2023-11-11 22:58:12 +0800

    feat(hm): update home.nix

---
+++ home-manager/home.nix
@@ -0,0 +1,70 @@
+{pkgs, ...}:
+let 
+  tex = with pkgs; texlive.combined.scheme-medium;
+in
+{
+  imports = [ 
+            ./modules/term
+            ./modules/desktop
+            ./modules/vcs
+  ];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+       "electron-24.8.6"
+      ];
+    };
+  };
+
+  home = {
+    stateVersion = "23.05";
+    packages = with pkgs; [
+      imv
+      mpv
+      eww
+      tex
+      btop
+      htop
+      fish
+      glow
+      swww
+      pypy3
+      conda
+      emacs
+      dunst
+      kitty
+      typst
+      pywal
+      gitui
+      vivid
+      rustup
+      ranger
+      logseq
+      deluge
+      nodejs
+      ffmpeg
+      httpie
+      waybar
+      bemenu
+      zoxide
+      zathura
+      pcmanfm
+      lazygit
+      pamixer
+      joshuto
+      obsidian
+      starship
+      nwg-look
+      wireshark
+      pavucontrol
+      go-musicfox
+      microsoft-edge
+      telegram-desktop
+      firefox-devedition
+      linux-wallpaperengine
+    ];
+  };
+
+}
