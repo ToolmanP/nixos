d8a1d56e3126eb7e109343f5a0fc81e5f095b8c4
Yiyang Wu <toolmanp@outlook.com>
2023-11-11 22:56:39 +0800

    feat(starship): add starship module

---
+++ home-manager/modules/term/starship.nix
@@ -0,0 +1,8 @@
+{
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+    settings = { aws = { symbol = "  "; }; buf = { symbol = " "; }; c = { symbol = " "; }; conda = { symbol = " "; }; dart = { symbol = " "; }; directory = { read_only = " 󰌾"; }; docker_context = { symbol = " "; }; elixir = { symbol = " "; }; elm = { symbol = " "; }; fossil_branch = { symbol = " "; }; git_branch = { symbol = " "; }; golang = { symbol = " "; }; guix_shell = { symbol = " "; }; haskell = { symbol = " "; }; haxe = { symbol = " "; }; hg_branch = { symbol = " "; }; hostname = { ssh_symbol = " "; }; java = { symbol = " "; }; julia = { symbol = " "; }; lua = { symbol = " "; }; memory_usage = { symbol = "󰍛 "; }; meson = { symbol = "󰔷 "; }; nim = { symbol = "󰆥 "; }; nix_shell = { symbol = " "; }; nodejs = { symbol = " "; }; os = { symbols = { Alpaquita = " "; Alpine = " "; Amazon = " "; Android = " "; Arch = " "; Artix = " "; CentOS = " "; Debian = " "; DragonFly = " "; Emscripten = " "; EndeavourOS = " "; Fedora = " "; FreeBSD = " "; Garuda = "󰛓 "; Gentoo = " "; HardenedBSD = "󰞌 "; Illumos = "󰈸 "; Linux = " "; Mabox = " "; Macos = " "; Manjaro = " "; Mariner = " "; MidnightBSD = " "; Mint = " "; NetBSD = " "; NixOS = " "; OpenBSD = "󰈺 "; OracleLinux = "󰌷 "; Pop = " "; Raspbian = " "; RedHatEnterprise = " "; Redhat = " "; Redox = "󰀘 "; SUSE = " "; Solus = "󰠳 "; Ubuntu = " "; Unknown = " "; Windows = "󰍲 "; openSUSE = " "; }; }; package = { symbol = "󰏗 "; }; pijul_channel = { symbol = " "; }; python = { symbol = " "; }; rlang = { symbol = "󰟔 "; }; ruby = { symbol = " "; }; rust = { symbol = " "; }; scala = { symbol = " "; }; };
+  };
+}
