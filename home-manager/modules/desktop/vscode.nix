d699179c69a57cd7b05cb242d5c5cd991d3d8731
Yiyang Wu <toolmanp@outlook.com>
2023-11-12 15:51:25 +0800

    feat(vscode): update vscode extensions

--- home-manager/modules/desktop/vscode.nix
+++ home-manager/modules/desktop/vscode.nix
@@ -1,6 +1,15 @@
-{
+{ pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    extensions = [];
+    package = pkgs.vscode-fhs;
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+      golang.go
+      arcticicestudio.nord-visual-studio-code
+      ms-toolsai.jupyter
+      ms-toolsai.vscode-jupyter-slideshow
+      ms-toolsai.vscode-jupyter-cell-tags
+      ms-toolsai.jupyter-renderers
+    ];
   };
 }
