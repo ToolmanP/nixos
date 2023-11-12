fb6acf1908feb489b3709844dbfdc7f8288c6bf1
Yiyang Wu <toolmanp@outlook.com>
2023-11-12 15:51:58 +0800

    feat(nvim): add lsp pkgs

--- home-manager/modules/term/nvim.nix
+++ home-manager/modules/term/nvim.nix
@@ -1,7 +1,18 @@
-{pkgs, ...}:
-{
+{ pkgs, ... }:
+let pypkg = ps: with ps; [ python-lsp-server autopep8 flake8 ];
+in {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    extraPackages = with pkgs; [
+      nil
+      nixfmt
+      gopls
+      stylua
+      lua-language-server
+      prettierd
+      deno
+      (python3.withPackages (pypkg))
+    ];
   };
 }
